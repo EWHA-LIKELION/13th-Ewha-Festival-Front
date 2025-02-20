c995d24a051ac7803ecdcd2a965608e8a0ac5a8d
s-uxun <serenasol@ewha.ac.kr>
2025-02-21 01:51:06 +0900

    feat: 로그인 페이지 퍼블리싱 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Test from './pages/test';
+import LoginPage from './pages/login/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Test />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
