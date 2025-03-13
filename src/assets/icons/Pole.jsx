abc6342488fc80df19c6b9bd5684889f4c8a0d91
s-uxun <serenasol@ewha.ac.kr>
2025-03-13 18:40:06 +0900

    feat: 메인 페이지 기본적인 배경 구현

---
+++ src/assets/icons/Pole.jsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+const SvgPole = props => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    fill='none'
+    viewBox='0 0 28 515'
+    {...props}
+  >
+    <path
+      fill='#012A1A'
+      fillRule='evenodd'
+      d='M0 2a2 2 0 0 1 2-2h24a2 2 0 0 1 2 2v24a2 2 0 0 1-2 2h-3v430h3a2 2 0 0 1 2 2v55H0v-55a2 2 0 0 1 2-2h4V28H2a2 2 0 0 1-2-2z'
+      clipRule='evenodd'
+    />
+  </svg>
+);
+export default SvgPole;
