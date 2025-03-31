d81481fcdb85d2314113f5050cacafbb852f9079
3olly <cy.kim@ewhain.net>
2025-03-31 18:06:04 +0900

    fix: 부스, 공연 상세페이지 구조 수정

--- src/pages/DetailPage/Show/ShowDetail.jsx
+++ src/pages/DetailPage/Show/ShowDetail.jsx
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import http from '@/api/http';
-
-import BoothHeader from '@/pages/DetailPage/Show/components/BoothHeader';
-import BoothInfo from '@/pages/DetailPage/Show/components/BoothInfo.jsx';
-import BoothButton from '@/pages/DetailPage/Show/components/BoothButton.jsx';
-import BoothMeta from '@/pages/DetailPage/Show/components/BoothMeta.jsx';
+import DetailTemplate from '@/pages/DetailPage/shared/components/DetailTemplate.jsx';
 import BoothTabs from '@/pages/DetailPage/Show/components/BoothTabs.jsx';
-import basicThumbnail from '@/pages/DetailPage/Booth/images/basicThumbnail.svg';
 
 const ShowDetail = () => {
-  const booth_id = 2; // ✅ 부스 목록 개발 끝나면 변경해주기
+  const booth_id = 2;
   const [boothData, setBoothData] = useState(null);
   const [operatingHours, setOperatingHours] = useState([]);
   const [thumbnailSrc, setThumbnailSrc] = useState(null);
@@ -21,79 +15,32 @@ const ShowDetail = () => {
     const fetchBoothData = async () => {
       try {
         const response = await http.get(`/shows/notices/${booth_id}/`);
-
         setBoothData(response.data.booth);
         setOperatingHours(response.data.operating_hours || []);
-        setThumbnailSrc(response.data.booth.thumbnail || basicThumbnail);
+        setThumbnailSrc(response.data.booth.thumbnail);
         setScrapState(response.data.is_scrap);
         setRole(response.data.booth.role || 'guest');
-      } catch (error) {
-        console.error('Error loading data:', error);
-      }
+      } catch (error) {}
     };
 
     fetchBoothData();
   }, []);
 
-  if (!boothData) return <p>Loading...</p>;
-
   return (
     <>
-      <BoothHeader role={role} />
-      <ThumbnailWrapper>
-        <BoothThumbnail
-          src={thumbnailSrc}
-          alt={boothData.name || '기본 이미지'}
-          onError={() => setThumbnailSrc(basicThumbnail)}
-        />
-      </ThumbnailWrapper>
-      <InfoWrapper>
-        <BoothInfo
-          name={boothData.name}
-          category={boothData.category}
-          description={boothData.description}
-        />
-        <BoothButton
-          contact={boothData.contact}
-          scrapCount={boothData.scrap_count}
-          scrapState={scrapState}
-          setScrapState={setScrapState}
-          boothId={booth_id}
-        />
-        <BoothMeta
-          location={boothData.formatted_location}
-          operatingHours={operatingHours}
-        />
-      </InfoWrapper>
-      <BoothTabs />
+      <DetailTemplate
+        boothData={boothData}
+        operatingHours={operatingHours}
+        thumbnailSrc={thumbnailSrc}
+        setThumbnailSrc={setThumbnailSrc}
+        scrapState={scrapState}
+        setScrapState={setScrapState}
+        boothId={booth_id}
+        role={role}
+        BoothTabsComponent={BoothTabs}
+      />
     </>
   );
 };
 
 export default ShowDetail;
-
-const ThumbnailWrapper = styled.div`
-  position: relative;
-  width: 100%;
-`;
-
-const BoothThumbnail = styled.img`
-  z-index: 0;
-  width: 100%;
-  height: 15rem;
-  object-fit: cover;
-  background: linear-gradient(
-    180deg,
-    rgba(0, 0, 0, 0.8) 35.78%,
-    rgba(0, 0, 0, 0) 100%
-  );
-`;
-
-const InfoWrapper = styled.div`
-  padding-left: 1.25rem;
-  padding-right: 1.25rem;
-  padding-top: 1rem;
-  padding-bottom: 2rem;
-  justify-content: flex-start;
-  background-color: white;
-`;
