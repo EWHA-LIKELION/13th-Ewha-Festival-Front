f1778e5484b50f2b23feb8c8249d3b2544882ab2
3olly <cy.kim@ewhain.net>
2025-05-03 17:27:31 +0900

    fix: 상세페이지 공통 요소 수정

--- src/pages/DetailPage/Show/components/Guestbook/BoothGuestbook.jsx
+++ src/pages/DetailPage/Show/components/Guestbook/BoothGuestbook.jsx
@@ -5,8 +5,8 @@ import ShowGuestbookStore, {
 } from '@/store/ShowGuestbookStore';
 import GuestbookItem from '@/pages/DetailPage/Show/components/Guestbook/GuestbookItem';
 import GuestbookInputBox from '@/pages/DetailPage/Show/components/Guestbook/GuestbookInputBox';
-import GuestbookDeleteModal from '@/pages/DetailPage/Show/modals/GuestbookDeleteModal';
-import noGuestBookImg from '@/pages/DetailPage/Booth/images/noGuestBook.svg';
+import GuestbookDeleteModal from '@/pages/DetailPage/shared/GuestbookDeleteModal';
+import noGuestBookImg from '@/assets/images/cloudBg.png';
 import http from '@/api/http';
 
 const BoothGuestbook = ({ boothId }) => {
@@ -74,7 +74,11 @@ const BoothGuestbook = ({ boothId }) => {
         ))
       ) : (
         <NoGuestbook>
-          <img src={noGuestBookImg} alt='방명록 없음' />
+          <NoGuestbookText>
+            등록된 방명록이 없어요.
+            <br />첫 방명록을 남겨볼까요?
+          </NoGuestbookText>
+          <NoGuestbookImage src={noGuestBookImg} alt='방명록 없음' />
         </NoGuestbook>
       )}
       <GuestbookInputBox
@@ -105,15 +109,32 @@ const GuestbookContainer = styled.div`
 `;
 
 const NoGuestbook = styled.div`
+  position: relative;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
-  width: 100%;
-  height: 100%;
+  background-color: white;
+  padding-top: 6.88rem;
+  padding-bottom: 5rem;
+  min-height: 60vh;
+  overflow: hidden;
+`;
 
-  img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
+const NoGuestbookText = styled.p`
+  color: var(--gray3, #787878);
+  ${({ theme }) => theme.fontStyles.regular_14pt};
+  text-align: center;
+  margin-bottom: 1rem;
+  z-index: 1;
+`;
+
+const NoGuestbookImage = styled.img`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  max-width: 100%;
+  height: auto;
+  object-fit: cover;
+  z-index: 0;
 `;
