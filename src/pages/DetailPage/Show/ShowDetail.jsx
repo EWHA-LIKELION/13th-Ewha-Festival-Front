c25b7115f1d526fa8bcb94ae0e3cd89346fe7b35
3olly <cy.kim@ewhain.net>
2025-05-12 07:00:30 +0900

    feat: 탭 하위요소 min-heignt 추가

--- src/pages/DetailPage/Show/ShowDetail.jsx
+++ src/pages/DetailPage/Show/ShowDetail.jsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import http from '@/api/http';
